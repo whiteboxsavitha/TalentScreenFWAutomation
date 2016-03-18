2c4a746bd3ef5ee58d6e55aa6c8284cd60ed2c4c
Savitha <kssavitha@yahoo.com>
2016-03-17 21:13:49 -0700

    All tests in the LoginTest Passed

---
+++ src/com/talentscreen/test/EmployeeSigupTest.java
@@ -0,0 +1,41 @@
+package com.talentscreen.test;
+
+import com.talentscreen.pages.SignUpPage;
+
+import com.talentscreen.base.BaseTest;
+import com.talentscreen.util.HtmlElement;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertEquals;
+/**
+ * Created by Savi on 3/11/2016.
+ */
+public class EmployeeSigupTest extends BaseTest{
+
+    private SignUpPage _ssup;
+    private String sheetName;
+
+    @BeforeTest
+    @Parameters({"sheetName"})
+    public void beforeClass(String sheetName) {
+        _ssup= new SignUpPage(d);
+        this.sheetName = sheetName;
+    }
+
+    @DataProvider(name = "users-data")
+    public Object[][] getUsers() throws Exception {
+        Object[][] data = excelUtils.getSimpleExcelData(d._configuration.DataFileName,sheetName);
+        System.out.print(data);
+        return data;
+    }
+   @Test(dataProvider = "users-data", enabled = true, groups = "Positive Test")
+    public void EmployeeSignupTest(String uname,String pwd, String confirmPw)throws IOException
+    {
+        String output = _ssup.performEmployeeSignup(uname, pwd, confirmPw);
+        assertEquals(output, "your sucessfully Register please log in");
+    }
+}
